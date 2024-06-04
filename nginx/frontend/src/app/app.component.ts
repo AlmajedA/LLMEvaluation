e5aa6a113928c69ed3c1f61da44f6b8878417f5e
Ammar Almajed <ammaralmajed7@gmail.com>
2024-06-04 04:25:14 +0300

    Final modification

--- nginx/frontend/src/app/app.component.ts
+++ nginx/frontend/src/app/app.component.ts
@@ -2,19 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './components/home/home.component';
 import { ChatDialogComponent } from './components/chat-dialog/chat-dialog.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, ChatDialogComponent, FormsModule, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, ChatDialogComponent, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = "frontend";
+  title = "LLMEvaluation";
   GPT35 = "GPT 3.5 Turbo";
   GPT4 = "GPT 4"
   llama = "Llama (Not Implemented Yet)"
